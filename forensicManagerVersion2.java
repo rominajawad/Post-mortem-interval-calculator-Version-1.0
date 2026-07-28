b71c5a1a5558df955383aff89c6e334daf72627a
Romina Jawad <rominajawad132@gmail.com>
2026-07-28 16:19:38 -0400

    Update and rename forensicManagerversion2.java to forensicManagerVersion2.java

--- forensicManagerVersion2.java
+++ forensicManagerVersion2.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class forensicManager {
+public class forensicManagerVersion2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
